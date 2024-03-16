fc434d0be3048d69385950365d90f17564b30a38
Vaibhavchawhan <vaibhavchawhan123@gmail.com>
2024-03-16 13:15:23 +0530

    update a1 project

---
+++ src/test/java/Basic_Jenkins/OrganizationsTest.java
@@ -0,0 +1,13 @@
+package Basic_Jenkins;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class OrganizationsTest {
+	@Test
+	public void CreateOrganizationsTest() {
+       WebDriver driver= new ChromeDriver();
+       driver.get("http://localhost:8888/");
+	}
+}
